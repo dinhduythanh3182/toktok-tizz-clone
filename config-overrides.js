b933b847f832e2d91598681bbbb0b76a63c664fb
DuyThanh <duythanhtizzer@gmail.com>
2022-05-09 22:09:07 +0700

    add-prettier

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+/* config-overrides.js */
+const { override,useBabelRc} = require("customize-cra");
+
+const path = require("path");
+
+module.exports = override(
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useBabelRc()
+);
